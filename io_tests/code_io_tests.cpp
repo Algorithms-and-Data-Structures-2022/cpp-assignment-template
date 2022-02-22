ad66e5c4a31e2c32081a69cfc6d8c52925b825c9
Ramil Safin <RamilSafNab1996@gmail.com>
2022-02-22 20:11:05 +0300

    Remove std::span dependency

--- io_tests/code_io_tests.cpp
+++ io_tests/code_io_tests.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
-#include <span>
 
 #include "assignment/code.hpp"
 
 int main(int argc, char** argv) {
-  const auto args = std::span(argv, argc);
-
-  for (int i = 1; i < args.size(); ++i) {
-    const int num = std::atoi(args[i]);
+  for (int i = 1; i < argc; ++i) {
+    const int num = std::atoi(argv[i]);
     std::cout << num * num << '\n';
   }
 
